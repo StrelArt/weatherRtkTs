b4bf104e4fe5b66a7d1a28676929d28fbf4dd076
StrelArt <streltsyn.artem@gmail.com>
2024-12-25 21:12:22 +0200

    Added keepUnusedDataFor

--- src/features/api/weatherApi.ts
+++ src/features/api/weatherApi.ts
@@ -7,10 +7,12 @@ export const weatherApi =  createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: base_url
     }),
+    keepUnusedDataFor: 3600*24*30,
     reducerPath: 'weatherApi',
     endpoints: builder => ({
         getWeatherByCity: builder.query<WeatherResponse, string>({
-            query: (city: string) => `?q=${city}&appid=${api_key}&units=metric`
+            query: (city: string) => `?q=${city}&appid=${api_key}&units=metric`,
+            // keepUnusedDataFor: 3600*24*30,
         })
     })
 
