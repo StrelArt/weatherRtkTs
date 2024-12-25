3be830642e3fa64cae53a25c7aeac883dee2fd6b
StrelArt <streltsyn.artem@gmail.com>
2024-12-25 22:14:25 +0200

    Added transformResponse

--- src/features/api/weatherApi.ts
+++ src/features/api/weatherApi.ts
@@ -1,18 +1,24 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {api_key, base_url} from "../../utils/constants.ts";
-import {WeatherResponse} from "../../utils/types";
+import {WeatherResponse, WeatherInfo} from "../../utils/types";
 
 export const weatherApi =  createApi({
 
     baseQuery: fetchBaseQuery({
         baseUrl: base_url
     }),
-    keepUnusedDataFor: 3600*24*30,
+    keepUnusedDataFor: 3600,
     reducerPath: 'weatherApi',
     endpoints: builder => ({
-        getWeatherByCity: builder.query<WeatherResponse, string>({
+        getWeatherByCity: builder.query<WeatherInfo, string>({
             query: (city: string) => `?q=${city}&appid=${api_key}&units=metric`,
-            // keepUnusedDataFor: 3600*24*30,
+            transformResponse: (response: WeatherResponse): WeatherInfo => ({
+                city: response.name,
+                country: response.sys.country,
+                temp: response.main.temp,
+                pressure: response.main.pressure,
+                sunset: response.sys.sunset,
+            })
         })
     })
 
